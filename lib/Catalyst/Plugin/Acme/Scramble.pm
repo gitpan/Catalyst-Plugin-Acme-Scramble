e42da5ecb80034c1f18d148727eb1b9999337cf5
Ashley Pond V — 바보 양키스 <ashley@cpan.org>
2006-01-18 21:55:56 +0000

    Import of ASHLEY/Catalyst-Plugin-Acme-Scramble-0.02 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-Acme-Scramble
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ASHLEY/Catalyst-Plugin-Acme-Scramble-0.02.tar.gz
    gitpan-cpan-author:       ASHLEY
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Plugin/Acme/Scramble.pm
+++ lib/Catalyst/Plugin/Acme/Scramble.pm
@@ -8,11 +8,11 @@ Catalyst::Plugin::Acme::Scramble - tset the budnos of lieibiglty and dstraneotme
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
